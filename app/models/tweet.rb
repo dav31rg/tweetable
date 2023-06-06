80ac4a18743547b8a3162915a8fa64524d3655e4
Davel <davel.reymundo@gmail.com>
2023-06-06 15:38:16 -0500

    Add asociations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
+  # Associations:
   belongs_to :user
+
+  has_many :likes, class_name: "like", foreign_key: "reference_id", dependent: :destroy
+  has_many :users, through: :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # Associations
+  has_many :tweets, dependent: :destroy
+
+  has_many :likes, class_name: "like", foreign_key: "reference_id", dependent: :destroy
+  has_many :tweets, through: :likes
 end
