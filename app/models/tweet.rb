914f1ee1b4d9413b357d481c7d0f6ef74fcf154d
Davel <davel.reymundo@gmail.com>
2023-06-23 19:11:25 -0500

    add asociations between Tweet an Like

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,16 @@
 class Tweet < ApplicationRecord
+  attribute :likes_count, :integer, default: 0
+
   # Associations:
   belongs_to :user
 
-  has_many :likes, class_name: "like", foreign_key: "reference_id", dependent: :destroy
+  has_many :likes, class_name: "Like", dependent: :destroy, inverse_of: :user
   has_many :users, through: :likes
+
+  has_many :replies, class_name: "Tweet",
+                    foreign_key: "replied_to_id",
+                    dependent: :destroy,
+                    inverse_of: "replied_to"
+
+  belongs_to :replied_to, class_name: "Tweet", optional: true
 end
