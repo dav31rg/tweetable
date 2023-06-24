c20a2719ad549d1d0f0713224344a5feee9a1cf8
Davel <davel.reymundo@gmail.com>
2023-06-23 19:08:45 -0500

    create model Tweet

--- db/migrate/20230606192615_create_tweets.rb
+++ db/migrate/20230606192615_create_tweets.rb
@@ -3,7 +3,7 @@ class CreateTweets < ActiveRecord::Migration[7.0]
     create_table :tweets do |t|
       t.text :body
       t.integer :replies_count
-      t.integer :likes_count
+      t.integer :likes_count, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
